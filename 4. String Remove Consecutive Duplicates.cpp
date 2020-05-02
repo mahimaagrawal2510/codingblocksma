4f94fe3e44378b617cec6e993a5ad07e865f3ea6
mahimaagrawal2510 <62229482+mahimaagrawal2510@users.noreply.github.com>
2020-05-01 22:42:24 -0700

    Create 4. String Remove Consecutive Duplicates.cpp

---
+++ 4. String Remove Consecutive Duplicates.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+void removeduplicates(char a[]){
+    int l=strlen(a);
+    if (l==1 or l==0){
+        return;
+    }
+    int prev=0;
+    for(int current =1;current<l;current++){
+        if (a[current] != a[prev]){
+            prev++;
+            a[prev]=a[current];
+        }
+    }
+    a[prev+1]='\0';
+    return;
+}
+int main(){
+    char a[1000];
+    //using getline
+    cin.getline(a,1000);
+    removeduplicates(a);
+     cout<<a;
+return 0;}
