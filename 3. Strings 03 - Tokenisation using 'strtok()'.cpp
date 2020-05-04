5c0b97c25084dab7052fb7c339320befb945b210
mahimaagrawal2510 <62229482+mahimaagrawal2510@users.noreply.github.com>
2020-05-03 23:48:28 -0700

    Create 3. Strings 03 - Tokenisation using 'strtok()'.cpp

---
+++ 3. Strings 03 - Tokenisation using 'strtok()'.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s[100]="Today is a beautiful day";
+    char *ptr = strtok(s," ");
+    cout << ptr<<endl;
+    while (ptr != NULL){
+        ptr= strtok(NULL," ");
+        cout<<ptr<<endl;
+    }
+    return 0;
+}
